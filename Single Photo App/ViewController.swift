29799cafaf9cc78b8676a04713d69b730d3f5fd1
Andrew Barber <edwardbar423@gmail.com>
2016-09-12 15:46:47 -0400

    first commit

---
+++ Single Photo App/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Single Photo App
+//
+//  Created by Andrew Barber on 9/12/16.
+//  Copyright © 2016 Invictus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var myPicture: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
